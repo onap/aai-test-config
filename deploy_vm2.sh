9c485b637845ae2c6881f0408834e908a003a275
Venkata Harish K Kajur <vk250x@att.com>
2017-09-28 15:51:43 +0000

    Fix the hbase startup issue using docker image
    Problem where running on two vms
    there seems to be connectivity issues
    Fixed that as well as be able to run locally
    
    Issue-ID: AAI-397
    Change-Id: I52df5f0c0d29e6295d94c04a009ededbaabe7fbe
    Signed-off-by: Venkata Harish K Kajur <vk250x@att.com>

--- deploy_vm2.sh
+++ deploy_vm2.sh
@@ -9,7 +9,7 @@ fi
 
 export MTU=$(/sbin/ifconfig | grep MTU | sed 's/.*MTU://' | sed 's/ .*//' | sort -n | head -1);
 export HBASE_IMAGE="${HBASE_IMAGE:-aaionap/hbase}";
-export HBASE_VERSION="${HBASE_VERSION:-1.0.0}";
+export HBASE_VERSION="${HBASE_VERSION:-1.2.0}";
 
 function wait_for_container() {
 
@@ -41,7 +41,8 @@ $DOCKER_COMPOSE_CMD stop
 $DOCKER_COMPOSE_CMD rm -f -v
 
 HBASE_CONTAINER_NAME=$($DOCKER_COMPOSE_CMD up -d aai.hbase.simpledemo.openecomp.org 2>&1 | grep 'Creating' | grep -v 'volume' | grep -v 'network' | awk '{ print $2; }' | head -1);
-#wait_for_container $HBASE_CONTAINER_NAME '^starting regionserver';
-wait_for_container $HBASE_CONTAINER_NAME 'HBase metrics system started';
+wait_for_container $HBASE_CONTAINER_NAME ' Started SelectChannelConnector@0.0.0.0:8085';
+wait_for_container $HBASE_CONTAINER_NAME ' Started SelectChannelConnector@0.0.0.0:8080';
+wait_for_container $HBASE_CONTAINER_NAME ' Started SelectChannelConnector@0.0.0.0:9095';
 
 $DOCKER_COMPOSE_CMD up -d aai.elasticsearch.simpledemo.openecomp.org
