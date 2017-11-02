9c56108ad5b0ae3a78143443914a0b7da1aa810d
Venkata Harish K Kajur <vk250x@att.com>
2017-11-02 15:43:17 -0400

    Fix the aaicron job to make sure resources is up
    Issue-ID: AAI-447
    Change-Id: I80876ddfd88c70e7cdf79bd81762f620439667ca
    Signed-off-by: Venkata Harish K Kajur <vk250x@att.com>

--- aaicron.sh
+++ aaicron.sh
@@ -2,6 +2,6 @@
 
 RESOURCES_COUNT=$(docker ps | grep 'testconfig_aai-resources.api.simpledemo.openecomp.org_1' | wc -l);
 
-if [ ${RESOURCES_COUNT} -eq 0 ]; then
+if [ ${RESOURCES_COUNT} -eq 1 ]; then
 	docker exec -u aaiadmin testconfig_aai-resources.api.simpledemo.openecomp.org_1 /opt/app/aai-resources/bin/dataSnapshot.sh >> /var/log/aaicron.log 2>&1	
 fi;
