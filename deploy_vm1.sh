3e70dcedd2fa73c064b435922be711bd2815cb8d
Venkata Harish K Kajur <vk250x@att.com>
2018-09-19 19:09:41 +0000

    Update haproxy for graphadmin
    and also remove the dynamic schema
    
    Issue-ID: AAI-1545
    Change-Id: I77cea3e42d94ba758ce11c5965df54a1851280ce
    Signed-off-by: Venkata Harish K Kajur <vk250x@att.com>

--- deploy_vm1.sh
+++ deploy_vm1.sh
@@ -150,46 +150,6 @@ function check_if_user_exists(){
         }
 }
 
-# Instead of duplicating the changes from aai-common
-# Clone the repo and extract the oxm and dbedgerules
-function retrieve_schema_edgerules(){
-
-    local current_branch=$(git rev-parse --abbrev-ref HEAD);
-    local current_dir=$(pwd);
-
-    local microservice_name=aai-common;
-    temp_dir=/tmp/${microservice_name}-$(uuidgen);
-    (
-        mkdir -p ${temp_dir} && cd ${temp_dir};
-        rm -r ${current_dir}/aai-schema
-
-        git init
-        git remote add origin https://gerrit.onap.org/r/aai/aai-common
-        git config core.sparsecheckout true
-
-        # Specifies which folders to checkout from the repo
-        # Limited to only the aai-resources as we don't need other folders from
-        # microservice deployment for this repository
-
-        echo "aai-schema/src/main/resources/**" >> .git/info/sparse-checkout
-
-        git fetch --depth=1 origin ${current_branch} && {
-            git checkout ${current_branch}
-        } || {
-            echo "Unable to find the branch ${current_branch} in aai-common, so using default branch ${default_branch}";
-            git fetch --depth=1 origin ${default_branch}
-            git checkout ${default_branch}
-        }
-
-        mkdir -p ${current_dir}/aai-schema
-        cp -R ${temp_dir}/aai-schema/src/main/resources/onap/ ${current_dir}/aai-schema
-
-        rm -rf ${temp_dir}
-    );
-}
-
-retrieve_schema_edgerules;
-
 docker pull ${DOCKER_REGISTRY}/onap/aai-graphadmin:${GRAPHADMIN_DOCKER_IMAGE_VERSION};
 docker tag $DOCKER_REGISTRY/onap/aai-graphadmin:${GRAPHADMIN_DOCKER_IMAGE_VERSION} $DOCKER_REGISTRY/onap/aai-graphadmin:latest;
 
